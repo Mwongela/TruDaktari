8f8a3569583815be8aa99150efb5ec5f9fa83c8b
ilabadmin <stephenmwongela@gmail.com>
2016-10-04 20:25:13 +0300

    adf

--- app/routes/constants.js
+++ app/routes/constants.js
@@ -1,6 +1,7 @@
 module.exports = {
 
 	doctor: {
+		URL		: 'http://medicalboard.co.ke/services/search.php',
 		Q		: 1,
 		KEY		: "bWVkX3JldGVudGlvbg==",
 		SALT	: "MQ==" 
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var http = require('http');
 var request = require('request');
+var _ = require('lodash');
+var htmlparser = require('html-to-json');
+
 var router = express.Router();
+
 
 var constants = require('./constants');
 
@@ -12,10 +16,67 @@ router.get('/', function(req, res, next) {
 
 router.post('/search/doctor', function(req, res) {
 
+    var query = req.body.query;
+
+    if (!query || query === "") {
+        res.status(401).json({
+            status: "Error",
+            message: "Please check your input"
+        });
+    }
+
+    var results = [];
+    var errors = [];
+    //var
+
+
+
     request({
+
+        method: "GET",
+        url: constants.doctor.URL,
+        headers: {},
+        qs: {
+            q: constants.doctor.Q,
+            salt: constants.doctor.SALT,
+            key: constants.doctor.KEY,
+            search: query
+        }
 
     }, function(err, response, body) {
 
+        if (err) {
+
+        }
+        //console.log(response);
+        //console.log(body);
+
+        htmlparser.parse(body, {results: function() {
+
+        	return this.map('table tbody tr', function($item) {
+        		
+        		var items = $item.find('td');
+        		//console.dir(items);
+
+        		for(var key in items) {
+        			if(items.hasOwnProperty(key)) {
+        				var item = items[key];
+        				console.dir(item);
+        			}
+        		}
+
+        		return {
+        			name: $item.text()
+        		}
+        	});
+
+        }}, function(err, result) {
+            //console.log(result);
+
+            return res.send(result);
+        });
+
+    
 
 
     });
